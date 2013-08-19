46794e5c05050d8c3140f4d33c389c9070629e18
Joseph Wecker <joseph.wecker@gmail.com>
2013-08-19 14:39:06 -0700

    Some more xml parsing done for rss poller (which is getting back-burnered for a while now)

--- lib/rss_poll.ex
+++ lib/rss_poll.ex
@@ -5,12 +5,45 @@ defmodule RssPoll do
 
   def fetch(rss_url) do
     case HTTPotion.get(rss_url, @user_agent) do
-      Response[body: body, status_code: status, headers: _headers] when status in 200..299 -> {:ok,body |> parse_xml}
-      response -> {:error, response}
+      Response[body: body, status_code: status, headers: _headers] when status in 200..299 ->
+        {parsed, _} = :binary.bin_to_list(body) |> :xmerl_scan.string([xmlbase: rss_url, space: :normalize])
+        {:ok, parsed}
+      response ->
+        {:error, response}
     end
   end
 
-  defp parse_xml(xml) do
-    xml
+  def parse(xml) do
+    defrecord :xmlDecl,      Record.extract(:xmlDecl,      from_lib: "xmerl/include/xmerl.hrl")
+    defrecord :xmlAttribute, Record.extract(:xmlAttribute, from_lib: "xmerl/include/xmerl.hrl")
+    defrecord :xmlNamespace, Record.extract(:xmlNamespace, from_lib: "xmerl/include/xmerl.hrl")
+    defrecord :xmlNsNode,    Record.extract(:xmlNsNode,    from_lib: "xmerl/include/xmerl.hrl")
+    defrecord :xmlElement,   Record.extract(:xmlElement,   from_lib: "xmerl/include/xmerl.hrl")
+    defrecord :xmlText,      Record.extract(:xmlText,      from_lib: "xmerl/include/xmerl.hrl")
+    defrecord :xmlComment,   Record.extract(:xmlComment,   from_lib: "xmerl/include/xmerl.hrl")
+    defrecord :xmlPI,        Record.extract(:xmlPI,        from_lib: "xmerl/include/xmerl.hrl")
+    defrecord :xmlDocument,  Record.extract(:xmlDocument,  from_lib: "xmerl/include/xmerl.hrl")
+    defrecord :xmlContext,   Record.extract(:xmlContext,   from_lib: "xmerl/include/xmerl.hrl")
+    defrecord :xmlObj,       Record.extract(:xmlObj,       from_lib: "xmerl/include/xmerl.hrl")
   end
+
+
+
+  # -record(xmlElement,{
+  # 	  name,			% atom()
+  # 	  expanded_name = [],	% string() | {URI,Local} | {"xmlns",Local}
+  # 	  nsinfo = [],	        % {Prefix, Local} | []
+  # 	  namespace=#xmlNamespace{},
+  # 	  parents = [],		% [{atom(),integer()}]
+  # 	  pos,			% integer()
+  # 	  attributes = [],	% [#xmlAttribute()]
+  # 	  content = [],
+  # 	  language = "",	% string()
+  # 	  xmlbase="",           % string() XML Base path, for relative URI:s
+  # 	  elementdef=undeclared % atom(), one of [undeclared | prolog | external | element]
+  # 	 }).
+
+  # defrecordp :xmlElement, name: :undefined, expanded_name: [], nsinfo: [], namespace: {:xmlNamespace, [], []}, parents: [], pos: :undefined, attributes: [], content: [], language: [], xmlbase: [], elementdef: :undeclared
+  #  {:ok, xml} = RssPoll.fetch "http://feeds.reuters.com/Counterparties?format=xml"
+  #  [i] = :xmerl_xpath.string('/rss/channel/item[1]/content:encoded',xml)
 end
